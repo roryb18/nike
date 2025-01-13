cea0c459db55c9d1661d005062fd1902a1b5b783
roryb18 <rory.buchan@hotmail.co.uk>
2025-01-13 15:49:25 +0000

    Updated tailwind install

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./public/*.{html,js}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      maxWidth: {
-        "8xl": "96rem",
-      },
-      spacing: {
-        128: "32rem",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
