99ce25bccf787c0fed53d0a028b0f8031b8a7d7b
roryb18 <rory.buchan@hotmail.co.uk>
2025-01-20 23:04:33 +0000

    Hero section styling complete

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,8 @@ import { navLinks } from '../constants';
 
 const Nav = () => {
   return (
-    <header className='padding-x py-8 absolute z-10 w-full bg-blue-100'>
-        <nav className='flex justify-between items-center max-container bg-blue-200'>
+    <header className='padding-x py-8 absolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-container'>
             <a href="/">
                 <img src={headerLogo} alt='Logo' width={130} height={29} />
             </a>
